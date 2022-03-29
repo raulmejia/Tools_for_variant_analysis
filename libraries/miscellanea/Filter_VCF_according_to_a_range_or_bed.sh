b7e6d9e18a281d7363259ab66370962bca4a34d6
raulmejia <r8a8m8p8@gmail.com>
2022-03-29 14:29:19 +0200

    eliminating redundant

--- libraries/miscellanea/Filter_VCF_according_to_a_range_or_bed.sh
+++
@@ -1,7 +0,0 @@
-# download your gatk
-# Path to your gatk executable
-
-mkdir -p $(dirname $your_output_file)
-
-bedtools intersect -a $your_inputvcf -b $Coordinates_in_a_bedfile > $your_output_file
-
